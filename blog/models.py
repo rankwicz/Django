d31d3803c51686ac77f8a6d28940331b4f5af9b7
Roman <ankwiczr@gmail.com>
2023-08-28 14:17:18 +0300

    class news

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+#чтобы брать текущее время для колоник ДАТА, нужно подключить
+from django.utils import timezone
+#привязать статью к определённому пользователю
+from django.contrib.auth.models import User
 
 # Create your models here.
+# файл для работы с базой данных
+# эти таблицы можно редактировать через панель администратора
+
+#создаём новую табличку. название класса любое
+class News(models.Model):
+    title = models.CharField('Название статьи',max_length=100)
+    text = models.TextField('Основной текст статьи')
+    date = models.DateTimeField(default=timezone.now)
+    autor = models.ForeignKey(User, on_delete=None)
